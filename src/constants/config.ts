ae29e78f21924fa470f31a477382051e2773a949
Ramakrishna Kamma <kammaramakrishna12@gmail.com>
2025-12-11 14:47:22 -0600

    Update environment variable names

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -5,7 +5,7 @@ const LocalConfig = {
     AUTOPREFIXER_GRID: process.env.AUTOPREFIXER_GRID,
     NEXT_PUBLIC_GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     NEXT_PUBLIC_VERCEL_TOKEN: process.env.VERCEL_TOKEN,
-    NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "#",
+    NEXT_PUBLIC_RESUME_URL: process.env.NEXT_PUBLIC_RESUME_URL || "#",
   },
 };
 
