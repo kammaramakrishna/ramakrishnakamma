5a0b916069fb60bcfcf9136e6e03854cbc58a36c
Ramakrishna Kamma <kammaramakrishna12@gmail.com>
2025-12-11 15:46:17 -0600

    Update Sentry details

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,14 +1,11 @@
 import dynamic from "next/dynamic";
 
 const PageBox = dynamic(() => import("@/components/core/PageBox"));
-const PaymentMainSection = dynamic(
-  () => import("@/components/payment/MainSection")
-);
 
 const Home = () => {
   return (
     <PageBox>
-      <PaymentMainSection />
+      <> </> {/* empty children */}
     </PageBox>
   );
 };
